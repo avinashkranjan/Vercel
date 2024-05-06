948a72cbae61da30df304b1be9a259002d4a5478
Avinash Ranjan <ranjan.avinash@hotmail.com>
2024-05-07 04:34:12 +0530

    minor fix: env file and morgan dependency added

--- upload-service/src/aws.ts
+++ upload-service/src/aws.ts
@@ -1,14 +1,20 @@
 import { S3 } from "aws-sdk";
 import fs from "fs";
+import { ACCESS_KEY_ID, SECRET_ACCESS_KEY, ENDPOINT } from "./util/secrets";
+
+const s3 = new S3({
+  accessKeyId: ACCESS_KEY_ID,
+  secretAccessKey: SECRET_ACCESS_KEY,
+  endpoint: ENDPOINT,
+});
 
 export const uploadFile = async (fileName: string, localFilePath: string) => {
   const fileContent = fs.readFileSync(localFilePath);
-  const response = await s3
+  await s3
     .upload({
       Body: fileContent,
       Bucket: "vercel",
       Key: fileName,
     })
     .promise();
-  console.log(response);
 };
--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -1,20 +1,30 @@
 import express from "express";
 import cors from "cors";
 import simpleGit from "simple-git";
-import { generate } from "./utils";
+import morgan from "morgan";
+
 import { getAllFiles } from "./file";
 import path from "path";
 import { uploadFile } from "./aws";
-import { createClient } from "redis";
-const publisher = createClient();
-publisher.connect();
+// import { createClient } from "redis";
+import * as dotenv from "dotenv";
+import { generate } from "./util/uniqueIdGen";
 
-const subscriber = createClient();
-subscriber.connect();
+// const publisher = createClient();
+// publisher.connect();
+
+// const subscriber = createClient();
+// subscriber.connect();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
+
+dotenv.config();
 
 app.post("/deploy", async (req, res) => {
   const repoUrl = req.body.repoUrl;
@@ -23,27 +33,31 @@ app.post("/deploy", async (req, res) => {
 
   const files = getAllFiles(path.join(__dirname, `output/${id}`));
 
+  console.log(
+    `Deploying ${files.length} files to S3 bucket. This may take a few minutes.`
+  );
+
   files.forEach(async (file) => {
     await uploadFile(file.slice(__dirname.length + 1), file);
   });
 
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  publisher.lPush("build-queue", id);
-  // INSERT => SQL
-  // .create =>
-  publisher.hSet("status", id, "uploaded");
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  // publisher.lPush("build-queue", id);
+  // // INSERT => SQL
+  // // .create =>
+  // publisher.hSet("status", id, "uploaded");
 
   res.status(200).json({
     id: id,
   });
 });
 
-app.get("/status", async (req, res) => {
-  const id = req.query.id;
-  const response = await subscriber.hGet("status", id as string);
-  res.json({
-    status: response,
-  });
-});
+// app.get("/status", async (req, res) => {
+//   const id = req.query.id;
+//   const response = await subscriber.hGet("status", id as string);
+//   res.json({
+//     status: response,
+//   });
+// });
 
 app.listen(3000);
